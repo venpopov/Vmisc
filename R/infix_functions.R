388e2205bfb8c6e6f055a941e9c21df148309ef4
venpopov <vencislav.popov@gmail.com>
2024-01-27 07:20:36 +0100

    add shortcut for paste0

--- R/infix_functions.R
+++ R/infix_functions.R
@@ -13,3 +13,17 @@
 `%a%` <- function(object,a) {
   attr(object, as.character(substitute(a)))
 }
+
+
+
+#' Shortcut for paste0
+#'
+#' @param a an R object to be converted to a character vector.
+#' @param b an R object to be converted to a character vector.
+#'
+#' @return String concatenation of a and b
+#' @export
+#'
+#' @examples
+#' "name" %+% 1:10
+`%+%` <- function(a, b) {paste0(a, b)}
